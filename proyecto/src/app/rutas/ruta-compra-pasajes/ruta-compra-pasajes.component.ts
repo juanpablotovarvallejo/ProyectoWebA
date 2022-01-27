1932de68d938bc63d21d316a62fff11bd476b2c8
Peterca <pierre.dolce@epn.edu.ec>
2022-01-27 00:18:06 -0500

    update/ creación de ruta compra

---
+++ proyecto/src/app/rutas/ruta-compra-pasajes/ruta-compra-pasajes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ruta-compra-pasajes',
+  templateUrl: './ruta-compra-pasajes.component.html',
+  styleUrls: ['./ruta-compra-pasajes.component.css']
+})
+export class RutaCompraPasajesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
