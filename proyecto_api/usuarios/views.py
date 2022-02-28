0021011232cd9efe0db24b329263ac20f0f954dc
Juan Pablo Tovar <juan.tovar@epn.edu.ec>
2022-02-28 16:08:45 -0500

    Feature /select usuarios

--- proyecto_api/usuarios/views.py
+++ proyecto_api/usuarios/views.py
@@ -5,3 +5,11 @@ from .serializer import UsuarioSerializer
 class UsuariosViewSet(viewsets.ModelViewSet):
     serializer_class =  UsuarioSerializer
     queryset = Usuario.objects.all()
+
+    def get_queryset(self):
+        queryset = Usuario.objects.all()
+        usercedula = self.request.query_params.get('cedula')
+        userpassword = self.request.query_params.get('contrasena')
+        if usercedula is not None:
+            queryset = Usuario.objects.filter(cedula=usercedula,constraseña=userpassword)
+        return queryset
