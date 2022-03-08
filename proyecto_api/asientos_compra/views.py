2d1a6917ee462c75316e8f470804150065242fcf
Juan Pablo Tovar <juan.tovar@epn.edu.ec>
2022-03-07 20:51:11 -0500

    Updates / nuevas consultas

--- proyecto_api/asientos_compra/views.py
+++ proyecto_api/asientos_compra/views.py
@@ -5,4 +5,11 @@ from .serializer import AsientoCompraSerializer
 class AsientosCompraViewSet(viewsets.ModelViewSet):
     serializer_class =  AsientoCompraSerializer
     queryset = Asiento_Compra.objects.all()
+
+    def get_queryset(self):
+        queryset = Asiento_Compra.objects.all()
+        compra = self.request.query_params.get('idCompra')
+        if compra is not None:
+            queryset = Asiento_Compra.objects.filter(orden_compra_id=compra)
+        return queryset
 # Create your views here.
--- proyecto_api/ordenes_compra/views.py
+++ proyecto_api/ordenes_compra/views.py
@@ -6,3 +6,9 @@ class OrdenesCompraViewSet(viewsets.ModelViewSet):
     serializer_class =  OrdenesCompraSerializer
     queryset = Orden_Compra.objects.all()
 
+    def get_queryset(self):
+        queryset = Orden_Compra.objects.all()
+        usuario = self.request.query_params.get('idUsuario')
+        if usuario is not None:
+            queryset = Orden_Compra.objects.filter(usuario_id=usuario)
+        return queryset
