8be6d8885201f705adde3607af7b9f0a64d1e87c
Juan Pablo Tovar <juan.tovar@epn.edu.ec>
2022-03-08 20:51:53 -0500

    Update / Nueva consulta

--- proyecto_api/viajes/views.py
+++ proyecto_api/viajes/views.py
@@ -5,3 +5,11 @@ from .serializer import ViajeSerializer
 class ViajesViewSet(viewsets.ModelViewSet):
     serializer_class =  ViajeSerializer
     queryset = Viaje.objects.all()
+        
+    def get_queryset(self):
+        queryset = Viaje.objects.all()
+        fecha = self.request.query_params.get('fecha')
+        hora = self.request.query_params.get('hora')
+        if fecha is not None:
+            queryset = Viaje.objects.filter(fecha__gte=fecha,hora__gte=hora)
+        return queryset
