067a4373ffb1624ec0829cd736351b7e1c7ea854
Peterca <pierre.dolce@epn.edu.ec>
2022-03-10 11:56:01 -0500

    fix/ arreglar formato de fecha

--- proyecto/src/app/rutas/ruta-inicio-sesion/ruta-inicio-sesion.component.ts
+++ proyecto/src/app/rutas/ruta-inicio-sesion/ruta-inicio-sesion.component.ts
@@ -15,33 +15,37 @@ export class RutaInicioSesionComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    console.log("esta logueado: "+this._authService.estaLogeado);
-    if(this._authService.estaLogeado){
+    console.log("esta logueado: " + this._authService.estaLogeado);
+    if (this._authService.estaLogeado) {
       console.log("usuario logueado: ");
       this.router.navigate(['/comprarPasajes']);
     }
   }
 
   @Input()
-  cedula = '1234567890';
+  cedula = '';
 
   @Input()
-  password = '12345678';
+  password = '';
 
   iniciarSesion() {
     if (this.cedula !== '' && this.password !== '') {
       this._authService.login(this.cedula, this.password).subscribe(
         (usuario: any) => {
           console.log(usuario);
-          this._authService.estaLogeado = true;
-          this._authService.saveSesion(usuario[0]);
-          this._authService.usuarioLogeado = usuario[0];
-          if (usuario[0].tipo_Usuario === 'Admin') {
-            console.log('Admin');
-            this.router.navigate(['/listarViajes']);
-          } else if (usuario[0].tipo_Usuario === 'usuario') {
-            console.log('usuario');
-            this.router.navigate(['/comprarPasajes']);
+          if (usuario.length > 0) {
+            this._authService.estaLogeado = true;
+            this._authService.saveSesion(usuario[0]);
+            this._authService.usuarioLogeado = usuario[0];
+            if (usuario[0].tipo_Usuario === 'Admin') {
+              console.log('Admin');
+              this.router.navigate(['/listarViajes']);
+            } else if (usuario[0].tipo_Usuario === 'usuario') {
+              console.log('usuario');
+              this.router.navigate(['/comprarPasajes']);
+            }
+          } else {
+            alert('Usuario o contraseña incorrectos');
           }
         }
       );
